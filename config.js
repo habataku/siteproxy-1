036030c76be12e060b169eed20c91116fe2d9b7b
Villager A <100194237+habataku@users.noreply.github.com>
2022-10-27 17:41:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.sqeven.vercel.app',
+    serverName: 'siteproxy-1-bufp2eah6-habatakutori.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
